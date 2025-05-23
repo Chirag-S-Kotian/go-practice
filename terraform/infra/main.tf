a0027dd6e20debc13a09b805060f9495c9395a71
Chirag-S-Kotian <ckotian117@gmail.com>
2025-05-23 18:06:00 +0530

    update DNS zone name and add CDN backend configuration for website

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -27,7 +27,7 @@ resource "google_compute_global_address" "website_ip" {
 
 # get the managed DNS zone for the domain name
 data "google_dns_managed_zone" "website_zone" {
-  name = "chirag117-zone"
+  name = "brave.chirag.cloud"
 }
 
 # add the ip to the DNS zone
@@ -37,4 +37,42 @@ resource "google_dns_record_set" "website_ip" {
   type         = "A"
   ttl          = 300
   rrdatas      = [google_compute_global_address.website_ip.address]
+}
+
+# add the bucket as a CDN backend
+resource "google_compute_backend_bucket" "website_backend" {
+  name        = "website-backend"
+  bucket_name = google_storage_bucket.website.name
+  description = "Backend for website"
+  enable_cdn  = true
+}
+
+# create url map for the website
+resource "google_compute_url_map" "website_map" {
+  name            = "website-map"
+  default_service = google_compute_backend_bucket.website_backend.self_link
+  host_rule {
+    hosts        = ["*"]
+    path_matcher = "allpaths"
+  }
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_bucket.website_backend.self_link
+  }
+}
+
+# create a global HTTP(S) load balancer
+resource "google_compute_target_http_proxy" "website_proxy" {
+  name    = "website-proxy"
+  url_map = google_compute_url_map.website_map.self_link
+}
+
+# create a global forwarding rule for the load balancer
+resource "google_compute_global_forwarding_rule" "website_forwarding_rule" {
+  name                  = "website-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.website_ip.address
+  ip_protocol           = "tcp"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.website_proxy.self_link
 }
